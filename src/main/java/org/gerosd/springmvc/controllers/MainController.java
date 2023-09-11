6958bb03aaf4a3d2aae9f8a815a97002c1fd067f
focks <focksikan@gmail.com>
2023-09-11 19:03:44 +0300

    Learning Model

--- src/main/java/org/gerosd/springmvc/controllers/MainController.java
+++ src/main/java/org/gerosd/springmvc/controllers/MainController.java
@@ -1,19 +1,35 @@
 package org.gerosd.springmvc.controllers;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String index(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    public String index(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname", required = false) String surname, Model model) {
 
-        System.out.println("Hello" + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
 
         return "index.html";
+    }
+
+    @ExceptionHandler
+
+    @GetMapping("/calculator")
+    public String calc(@RequestParam("a") double a, @RequestParam("b") double b, @RequestParam("condition") String condition, Model model) {
+        double result = 0;
+        switch (condition) {
+            case "division" -> result = a / b;
+            case "subtract" -> result = a - b;
+            case "multiplication" -> result = a * b;
+            case "addiction" -> result = a + b;
+        }
+        model.addAttribute("result", result);
+
+        return "homework/calculator.html";
     }
 
     @GetMapping("/hello")
