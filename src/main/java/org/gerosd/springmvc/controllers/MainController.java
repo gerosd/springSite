b1425b68c5e5030410ae8c74a3e013668b156985
focks <focksikan@gmail.com>
2023-09-11 18:23:45 +0300

    delete application.properties

--- src/main/java/org/gerosd/springmvc/controllers/MainController.java
+++ src/main/java/org/gerosd/springmvc/controllers/MainController.java
@@ -1,18 +1,24 @@
 package org.gerosd.springmvc.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String index() {
-        return "/WEB-INF/views/index.html";
+    public String index(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("Hello" + name + " " + surname);
+
+        return "index.html";
     }
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "/WEB-INF/views/hello_world.html";
+        return "hello_world.html";
     }
 
 }
